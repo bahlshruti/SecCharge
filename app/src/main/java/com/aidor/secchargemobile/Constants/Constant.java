f07172227f10768744cfaf100bf57a5dc2c28be3
sbahl <sbahl087@uottawa.ca>
2018-12-26 18:26:53 -0500

    creating auto mode page

--- app/src/main/java/com/aidor/secchargemobile/Constants/Constant.java
+++ app/src/main/java/com/aidor/secchargemobile/Constants/Constant.java
@@ -13,7 +13,7 @@ public class Constant {
     public static final List<String> positiveArray = Arrays.asList("yes","yea","yep","yeah");
     public static final List<String> negativeArray = Arrays.asList("no","nope");
     public static final List<String> option_1 = Arrays.asList("1","one");
-    public static final List<String> option_2 = Arrays.asList("2","two","too");
+    public static final List<String> option_2 = Arrays.asList("2","two","too","do");
     public static final List<String> option_3 = Arrays.asList("3","three");
     public static final List<String> option_4 = Arrays.asList("4","four");
     public static final List<String> option_5 = Arrays.asList("5","five");
